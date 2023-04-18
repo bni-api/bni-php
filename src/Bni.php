a499db1db30e27fb3939e830e6385d9f58b93682
ramadhnkahfix <ramadhani.k45@gmail.com>
2023-04-18 09:55:50 +0700

    Change Sandbox Dev base url

--- src/Bni.php
+++ src/Bni.php
@@ -22,7 +22,7 @@ class Bni
     
     const DEV_BASE_URL = 'https://newapidev.bni.co.id:8066';
     const SANDBOX_BASE_URL = "https://sandbox.bni.co.id";
-    const SANDBOX_DEV_BASE_URL = "http://localhost:8080";
+    const SANDBOX_DEV_BASE_URL = "https://sb-dev.harismawan.com";
     const UAT_BASE_URL = 'https://newapidev.bni.co.id:8065';
     const PRODUCTION_BASE_URL = "https://api.bni.co.id";
 
